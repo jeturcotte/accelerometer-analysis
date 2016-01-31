43530dad8159a3b6caff9b3270eab3dbedbf0d93
jeturcotte <joshua.eric.turcotte@gmail.com>
2016-01-31 14:10:28 -0500

    nearly there on first part

--- run_analysis.R
+++ run_analysis.R
@@ -20,10 +20,12 @@ if (!file.exists("./resulting_data")) {
 }
 
 # step 1a: collapse training and test SUBJECTS into one, and rename
+#    ... further, turn the subjects IDs into factors
 subjects_of_tests <- read.table(subject_test_file)
 subjects_of_training <- read.table(subject_train_file)
 all_subjects <- rbind(subjects_of_tests, subjects_of_training)
-names(all_subjects)[1] <- "subject_id" 
+names(all_subjects)[1] <- "subject" 
+all_subjects$subject <- as.factor(all_subjects$subject)
 rm(subjects_of_tests, subjects_of_training) # memory management
 
 # step 1b: collapse training and test X measures into one
@@ -45,9 +47,9 @@ rm(activity_of_tests, activity_of_training) # memory management
 # step 1d: get the NAMES of the activity types and nix the IDs
 #    ... yes, technically this is STEP 3
 labels <- read.table(activity_labels_file)
-names(labels) <- c("activity_id","activity_name")
+names(labels) <- c("activity_id","activity")
 all_activity <- merge(all_activity, labels, by.x="activity_id", by.y="activity_id")
-all_activity <- all_activity[,2]
+all_activity$activity_id <- NULL
 rm(labels) # memory management 
 
 # step 2a: isolate 'std' and 'mean' columns from measures
@@ -56,18 +58,21 @@ rm(labels) # memory management
 features <- read.table(features_file)
 preserve_columns <- grepl("(std|mean)\\(", features$V2)
 column_names <- grep("(std|mean)\\(", features$V2, value=TRUE)
+column_names <- c(column_names, 'session')
 all_measures <- all_measures[,preserve_columns]
 
-# step 2b: now lets rename those dang columns
-#names(all_measures) <- gsub("V","",names(all_measures))
-#lapply(seq(all_measures), function (column_number) {
-#     print(paste(column_number, column_names[column_number])) 
-#     names(all_measures)[column_number] <- column_names[column_number]
-#})
+# step 4a: get the NAMES of our columns in place
+#    ... yes, sorry, steps 2, 3, and 4 all out of order
+names(all_measures) <- lapply(column_names, function(n){ n })
 
 # step 2c: now, lets put all the data together
 all_data <- cbind(all_subjects, all_activity, all_measures)
 rm(all_subjects, all_activity, all_measures) # memory management
-print(str(all_data))
 
-# step 4a:  
+# step 4a: now, we must narrow/melt this data heavily
+all_data <- melt(all_data, id=c("subject","activity","session"), variable.name="measure_type", value.name="observation")
+
+# step 4b: determine if each new row is std or mean
+all_data$observation_type <- as.factor(ifelse(grepl("-std\\(",all_data$measure_type), "std", "mean"))
+
+
